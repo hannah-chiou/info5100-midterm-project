d344a0dcd2848ff8f1d3c807fb7327b27edbde08
merinagodly4 <godly.m@northeastern.edu>
2026-03-01 15:53:58 -0500

    Update UserAccount.java

--- ProfileWorkareaExample 3/ProfileWorkareaExample/src/Business/UserAccounts/UserAccount.java
+++ ProfileWorkareaExample 3/ProfileWorkareaExample/src/Business/UserAccounts/UserAccount.java
@@ -71,19 +71,7 @@ public class UserAccount {
         public String getUsername() {
         return username;
     }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-        
+     
         public void setLastAccess(LocalDateTime lastAccess) {
             this.lastAccess = lastAccess;
         }
